5213fbfb29eb974c8e42a273b4d9d09fec1e207f
VoidedWarranty <ianmpcarr@gmail.com>
2020-11-11 21:15:50 -0500

    fuck mock trial test 1

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = 016ef9300233dded8d0238cec3;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
+      botRegex = /^\/mock trial$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
