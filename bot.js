5a68cc41783e00e97b4e21992bda79e40191c066
VoidedWarranty <ianmpcarr@gmail.com>
2020-11-17 15:11:14 -0500

    added postCompliment() back in because I'm stupid

--- bot.js
+++ bot.js
@@ -68,6 +68,41 @@ function postMessage() {
   botReq.end(JSON.stringify(body));
 }
 
+function postCompliment(name) {
+  var botResponse, options, body, botReq;
+
+  botResponse = getCompliment(name);
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
 function postInsult(name) {
   var botResponse, options, body, botReq;
 
