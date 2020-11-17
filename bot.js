0ea4364f7546855c7c402d41ca6ae02ab7f8f1a8
VoidedWarranty <ianmpcarr@gmail.com>
2020-11-17 01:04:46 -0500

    test response

--- bot.js
+++ bot.js
@@ -6,13 +6,19 @@ var botID = "79e68331dba0dddfb1c4da512f"; //test bot
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^.*mock trial.*$/i;
+      botRegex = /^.*mock trial.*$/i
+      complimentRegex = /^munchbot, compliment.*$/i;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
-  } else {
+  } 
+  else if(request.text && complimentRegex.test(request.text)){
+    this.res.writeHead(200);
+    postCompliment();
+    this.res.end();
+    else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
@@ -23,6 +29,41 @@ function postMessage() {
   var botResponse, options, body, botReq;
 
   botResponse = "https://imgur.com/a/Xx4g4x6";
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function postCompliment() {
+  var botResponse, options, body, botReq;
+
+  botResponse = "this is a test response!";
 
   options = {
     hostname: 'api.groupme.com',
