8750b61254bc4d2f3af1911cf49b0bf11d39ac68
VoidedWarranty <ianmpcarr@gmail.com>
2020-11-17 21:31:06 -0500

    increase delay again

--- bot.js
+++ bot.js
@@ -113,7 +113,7 @@ function postMagic(request) {
   });
   botReq1.end(JSON.stringify(body1));
 
-  setTimeout(console.log('brief timeout'), 500);
+  setTimeout(console.log('brief timeout'), 1000);
 
   botReq2 = HTTPS.request(options, function(res) {
       if(res.statusCode == 202) {
